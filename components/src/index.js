1fdc298a146da3bd2f85b8e1308681e90f74044c
S.i.m.o.n <hoanvutt@gmail.com>
2021-10-25 14:43:14 +0700

    Add small smaple

--- components/src/index.js
+++ components/src/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there!</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sammy 
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 PM </span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
