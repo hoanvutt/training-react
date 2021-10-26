92f89eb18a662c410adfe9210694d5e87226d3c4
S.i.m.o.n <hoanvutt@gmail.com>
2021-10-26 14:30:55 +0700

    Add a approval card block

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Simon" />
+      <ApprovalCard />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Post comment 1"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        content="Post comment 2"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Simon"
+        timeAgo="Yesterday at 5:00PM"
+        content="Post comment 3"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
