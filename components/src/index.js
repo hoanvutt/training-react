c0fd788d123fc82c258af56f4eca331f287a97ca
S.i.m.o.n <hoanvutt@gmail.com>
2021-10-26 14:32:32 +0700

    Move Comment Detail to Approval Card

--- components/src/index.js
+++ components/src/index.js
@@ -7,13 +7,14 @@ import faker from 'faker';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Post comment 1"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Post comment 1"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
       <CommentDetail
         author="Alex"
         timeAgo="Today at 2:00PM"
