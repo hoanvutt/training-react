2a143b71bae1bcbf299782aa6a8879a015cf092f
S.i.m.o.n <hoanvutt@gmail.com>
2021-10-28 10:08:37 +0700

    Function componentDidMount()

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+  constructor(props) {
+    super(props);
 
-    return <div>Latitude: </div>;
+    this.state = { lat: null, errorMessage: '' };
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  componentDidUpdate() {
+    console.log('My component was just updated - it rendered!');
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Lat: {this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
